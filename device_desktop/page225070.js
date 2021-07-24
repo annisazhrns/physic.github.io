22f24b0e246e784d31cdea782e58607c0b1ef683
annisazhrns <87883745+annisazhrns@users.noreply.github.com>
2021-07-24 08:12:40 +0700

    Add files via upload

---
+++ device_desktop/page225070.js
@@ -0,0 +1,26 @@
+DesktopResponsive={"1009":{
+"pageLayer":{"w":800,"h":1035,"bgColor":"#ffffff","bgImage":"images/cisco_society_twitter_1200x600_flat.gif","bgSize":"1200px 600px","bgRepeat":"no-repeat"}
+,
+"image225072":{"x":0,"y":0,"w":800,"h":1035,"bOffBottom":0,"i":"images/certificate.png"}
+,
+"text225076":{"x":64,"y":252,"w":653,"h":113,"txtscale":100,"bOffBottom":0,"i":"images/text225076.png"}
+,
+"text225078":{"x":92,"y":410,"w":600,"h":80,"txtscale":100,"bOffBottom":0,"i":"images/text225078.png"}
+,
+"text225080":{"x":92,"y":498,"w":600,"h":62,"txtscale":100,"bOffBottom":0}
+,
+"text225082":{"x":94,"y":575,"w":600,"h":76,"txtscale":100,"bOffBottom":0,"i":"images/text225082.png"}
+,
+"text225088":{"x":99,"y":775,"w":600,"h":83,"txtscale":100,"bOffBottom":0}
+,
+"text228605":{"x":97,"y":692,"w":600,"h":83,"txtscale":100,"bOffBottom":0}
+,
+"text228342":{"x":100,"y":880,"w":600,"h":74,"txtscale":100,"bOffBottom":0}
+,
+"RCDResetQuestion":function(){
+try{if(window.dragMgr)window.dragMgr.clearDropZones();}catch(e){if(e&&e.message)console.log(e.message);}
+}
+,
+"RCDResultResize":function(){}
+,"preload":['images/cisco_society_twitter_1200x600_flat.gif','images/certificate.png','images/text225076.png','images/text225078.png','images/text225082.png']
+}}
