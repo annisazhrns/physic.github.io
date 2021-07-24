b6d685b3d3453624f6d25bc35e6e71dce9baf29c
annisazhrns <87883745+annisazhrns@users.noreply.github.com>
2021-07-24 08:29:31 +0700

    Add files via upload

---
+++ device_tablet/page2.js
@@ -0,0 +1,72 @@
+TabletResponsive={"1009":{
+"pageLayer":{"w":900,"h":662,"bgColor":"#ffffff","bgImage":"images/cisco_society_twitter_1200x600_flat.gif","bgSize":"1200px 600px","bgRepeat":"no-repeat"}
+,
+"button212887":{"x":735,"y":245,"w":47.000000,"h":47.000000,"stylemods":[{"sel":"div.button212887Text","decl":" { position:fixed; left:2px; top:2px; width:42px; height:42px;}"},{"sel":"span.button212887Text","decl":" { display:table-cell; position:relative; width:42px; height:42px; vertical-align:middle; text-align:center; line-height:16px; font-size:16px; font-family:\"Lucida Sans Unicode\"; color:#ffffff;}"}],"bOffBottom":0  ,"td": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAC8AAAAvCAYAAABzJ5OsAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAgSURBVGhD7cExAQAAAMKg9U9tCy8gAAAAAAAAAAB4qgEiswABwk0nKQAAAABJRU5ErkJggg==" ,"tdO": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAC8AAAAvCAYAAABzJ5OsAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAgSURBVGhD7cExAQAAAMKg9U9tCy8gAAAAAAAAAAB4qgEiswABwk0nKQAAAABJRU5ErkJggg==" ,"tdD": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAC8AAAAvCAYAAABzJ5OsAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAgSURBVGhD7cExAQAAAMKg9U9tCy8gAAAAAAAAAAB4qgEiswABwk0nKQAAAABJRU5ErkJggg==" ,"tdDi": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAC8AAAAvCAYAAABzJ5OsAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAgSURBVGhD7cExAQAAAMKg9U9tCy8gAAAAAAAAAAB4qgEiswABwk0nKQAAAABJRU5ErkJggg=="  ,"fd": "images/mull.png" ,"fdO": "images/mull.png" ,"fdD": "images/mull.png" ,"fdDi": "images/mull.png" ,"p": "M 0.000000 0.000000 L 46.000000 0.000000 L 46.000000 46.000000 L 0.000000 46.000000 L 0.000000 0.000000 z"}
+,
+"button176332":{"x":686,"y":252,"w":34.000000,"h":34.000000,"stylemods":[{"sel":"div.button176332Text","decl":" { position:fixed; left:2px; top:2px; width:29px; height:29px;}"},{"sel":"span.button176332Text","decl":" { display:table-cell; position:relative; width:29px; height:29px; vertical-align:middle; text-align:center; line-height:16px; font-size:16px; font-family:\"Lucida Sans Unicode\"; color:#ffffff;}"}],"bOffBottom":0  ,"td": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACIAAAAiCAYAAAA6RwvCAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAbSURBVFhH7cExAQAAAMKg9U/tawggAAAAADg1EjIAARhcUxMAAAAASUVORK5CYII=" ,"tdO": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACIAAAAiCAYAAAA6RwvCAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAbSURBVFhH7cExAQAAAMKg9U/tawggAAAAADg1EjIAARhcUxMAAAAASUVORK5CYII=" ,"tdD": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACIAAAAiCAYAAAA6RwvCAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAbSURBVFhH7cExAQAAAMKg9U/tawggAAAAADg1EjIAARhcUxMAAAAASUVORK5CYII=" ,"tdDi": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACIAAAAiCAYAAAA6RwvCAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAbSURBVFhH7cExAQAAAMKg9U/tawggAAAAADg1EjIAARhcUxMAAAAASUVORK5CYII="  ,"fd": "images/power-off.png" ,"fdO": "images/power-off.png" ,"fdD": "images/power-off.png" ,"fdDi": "images/power-off.png" ,"p": "M 0.000000 0.000000 L 33.000000 0.000000 L 33.000000 33.000000 L 0.000000 33.000000 L 0.000000 0.000000 z"}
+,
+"button176281":{"x":796,"y":251,"w":34.000000,"h":34.000000,"stylemods":[{"sel":"div.button176281Text","decl":" { position:fixed; left:2px; top:2px; width:29px; height:29px;}"},{"sel":"span.button176281Text","decl":" { display:table-cell; position:relative; width:29px; height:29px; vertical-align:middle; text-align:center; line-height:16px; font-size:16px; font-family:\"Lucida Sans Unicode\"; color:#ffffff;}"}],"bOffBottom":0  ,"td": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACIAAAAiCAYAAAA6RwvCAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAbSURBVFhH7cExAQAAAMKg9U/tawggAAAAADg1EjIAARhcUxMAAAAASUVORK5CYII=" ,"tdO": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACIAAAAiCAYAAAA6RwvCAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAbSURBVFhH7cExAQAAAMKg9U/tawggAAAAADg1EjIAARhcUxMAAAAASUVORK5CYII=" ,"tdD": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACIAAAAiCAYAAAA6RwvCAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAbSURBVFhH7cExAQAAAMKg9U/tawggAAAAADg1EjIAARhcUxMAAAAASUVORK5CYII=" ,"tdDi": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACIAAAAiCAYAAAA6RwvCAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAbSURBVFhH7cExAQAAAMKg9U/tawggAAAAADg1EjIAARhcUxMAAAAASUVORK5CYII="  ,"fd": "images/information.png" ,"fdO": "images/information.png" ,"fdD": "images/information.png" ,"fdDi": "images/information.png" ,"p": "M 0.000000 0.000000 L 33.000000 0.000000 L 33.000000 33.000000 L 0.000000 33.000000 L 0.000000 0.000000 z"}
+,
+"video258195":{"x":-69,"y":78,"w":64,"h":64,"bOffBottom":0}
+,
+"audio258597":{"x":292,"y":211,"w":300,"h":24,"bOffBottom":0}
+,
+"audio258616":{"x":292,"y":211,"w":300,"h":24,"bOffBottom":0}
+,
+"anim241197":{"x":0,"y":0,"w":900,"h":500,"bOffBottom":0}
+,
+"shape24400":{"x":23,"y":423,"w":419.000000,"h":58.000000,"stylemods":[{"sel":"div.shape24400Text","decl":" { position:fixed; left:2px; top:2px; width:414px; height:53px;}"},{"sel":"span.shape24400Text","decl":" { display:table-cell; position:relative; width:414px; height:53px; vertical-align:middle; text-align:center; line-height:13px; font-size:13px; font-family:\"Trebuchet MS\"; font-weight:bold; color:#000000;}"}],"bOffBottom":0 ,"td": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAaMAAAA6CAYAAAAdizbLAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAB0SURBVHhe7cEBAQAAAIIg/69uSEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA52p8AQABfiZctwAAAABJRU5ErkJggg=="  ,"fd": "" ,"p": "M 0.000000 0.000000 L 418.000000 0.000000 L 418.000000 57.000000 L 0.000000 57.000000 L 0.000000 0.000000 z"}
+,
+"image24257":{"x":46,"y":397,"w":102,"h":98,"bOffBottom":0,"i":"images/unj.png"}
+,
+"text24431":{"x":193,"y":43,"w":517,"h":94,"txtscale":100,"bOffBottom":0}
+,
+"text24677":{"x":150,"y":418,"w":611,"h":76,"txtscale":100,"bOffBottom":0}
+,
+"text24716":{"x":611,"y":189,"w":274,"h":49,"txtscale":100,"bOffBottom":0}
+,
+"entry26092":{"x":680,"y":212,"w":150,"h":27,"fsize":16,"bOffBottom":0}
+,
+"RCDResetQuestion":function(){
+try{if(window.dragMgr)window.dragMgr.clearDropZones();}catch(e){if(e&&e.message)console.log(e.message);}
+}
+,
+"RCDResultResize":function(){}
+,"preload":['images/cisco_society_twitter_1200x600_flat.gif']
+},
+"785":{
+"pageLayer":{"w":785,"h":1000,"bgColor":"#ffffff","bgImage":"images/cisco_society_twitter_1200x600_flat.gif","bgSize":"1047px 523px","bgRepeat":"no-repeat"}
+,
+"button212887":{"x":641,"y":245,"w":41.000000,"h":41.000000,"stylemods":[{"sel":"div.button212887Text","decl":" { position:fixed; left:2px; top:2px; width:36px; height:36px;}"},{"sel":"span.button212887Text","decl":" { display:table-cell; position:relative; width:36px; height:36px; vertical-align:middle; text-align:center; line-height:13px; font-size:13px; font-family:\"Lucida Sans Unicode\"; color:#ffffff;}"}],"bOffBottom":0  ,"td": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACkAAAApCAYAAACoYAD2AAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAeSURBVFhH7cExAQAAAMKg9U9tB28gAAAAAAAA+NQAGm0AAWCjjlsAAAAASUVORK5CYII=" ,"tdO": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACkAAAApCAYAAACoYAD2AAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAeSURBVFhH7cExAQAAAMKg9U9tB28gAAAAAAAA+NQAGm0AAWCjjlsAAAAASUVORK5CYII=" ,"tdD": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACkAAAApCAYAAACoYAD2AAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAeSURBVFhH7cExAQAAAMKg9U9tB28gAAAAAAAA+NQAGm0AAWCjjlsAAAAASUVORK5CYII=" ,"tdDi": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACkAAAApCAYAAACoYAD2AAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAeSURBVFhH7cExAQAAAMKg9U9tB28gAAAAAAAA+NQAGm0AAWCjjlsAAAAASUVORK5CYII="  ,"fd": "images/mull.png" ,"fdO": "images/mull.png" ,"fdD": "images/mull.png" ,"fdDi": "images/mull.png" ,"p": "M 0.000000 0.000000 L 40.000000 0.000000 L 40.000000 40.000000 L 0.000000 40.000000 L 0.000000 0.000000 z"}
+,
+"button176332":{"x":598,"y":252,"w":30.000000,"h":30.000000,"stylemods":[{"sel":"div.button176332Text","decl":" { position:fixed; left:2px; top:2px; width:25px; height:25px;}"},{"sel":"span.button176332Text","decl":" { display:table-cell; position:relative; width:25px; height:25px; vertical-align:middle; text-align:center; line-height:13px; font-size:13px; font-family:\"Lucida Sans Unicode\"; color:#ffffff;}"}],"bOffBottom":0  ,"td": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAaSURBVEhL7cExAQAAAMKg9U/tbwYgAAAAODUOLgABSH/AfwAAAABJRU5ErkJggg==" ,"tdO": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAaSURBVEhL7cExAQAAAMKg9U/tbwYgAAAAODUOLgABSH/AfwAAAABJRU5ErkJggg==" ,"tdD": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAaSURBVEhL7cExAQAAAMKg9U/tbwYgAAAAODUOLgABSH/AfwAAAABJRU5ErkJggg==" ,"tdDi": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAaSURBVEhL7cExAQAAAMKg9U/tbwYgAAAAODUOLgABSH/AfwAAAABJRU5ErkJggg=="  ,"fd": "images/power-off.png" ,"fdO": "images/power-off.png" ,"fdD": "images/power-off.png" ,"fdDi": "images/power-off.png" ,"p": "M 0.000000 0.000000 L 29.000000 0.000000 L 29.000000 29.000000 L 0.000000 29.000000 L 0.000000 0.000000 z"}
+,
+"button176281":{"x":694,"y":251,"w":30.000000,"h":30.000000,"stylemods":[{"sel":"div.button176281Text","decl":" { position:fixed; left:2px; top:2px; width:25px; height:25px;}"},{"sel":"span.button176281Text","decl":" { display:table-cell; position:relative; width:25px; height:25px; vertical-align:middle; text-align:center; line-height:13px; font-size:13px; font-family:\"Lucida Sans Unicode\"; color:#ffffff;}"}],"bOffBottom":0  ,"td": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAaSURBVEhL7cExAQAAAMKg9U/tbwYgAAAAODUOLgABSH/AfwAAAABJRU5ErkJggg==" ,"tdO": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAaSURBVEhL7cExAQAAAMKg9U/tbwYgAAAAODUOLgABSH/AfwAAAABJRU5ErkJggg==" ,"tdD": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAaSURBVEhL7cExAQAAAMKg9U/tbwYgAAAAODUOLgABSH/AfwAAAABJRU5ErkJggg==" ,"tdDi": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAaSURBVEhL7cExAQAAAMKg9U/tbwYgAAAAODUOLgABSH/AfwAAAABJRU5ErkJggg=="  ,"fd": "images/information.png" ,"fdO": "images/information.png" ,"fdD": "images/information.png" ,"fdDi": "images/information.png" ,"p": "M 0.000000 0.000000 L 29.000000 0.000000 L 29.000000 29.000000 L 0.000000 29.000000 L 0.000000 0.000000 z"}
+,
+"video258195":{"x":-61,"y":78,"w":56,"h":56,"bOffBottom":0}
+,
+"audio258597":{"x":255,"y":211,"w":300,"h":24,"bOffBottom":0}
+,
+"audio258616":{"x":255,"y":211,"w":300,"h":24,"bOffBottom":0}
+,
+"anim241197":{"x":0,"y":0,"w":785,"h":436,"bOffBottom":0}
+,
+"shape24400":{"x":20,"y":423,"w":366.000000,"h":51.000000,"stylemods":[{"sel":"div.shape24400Text","decl":" { position:fixed; left:2px; top:2px; width:361px; height:46px;}"},{"sel":"span.shape24400Text","decl":" { display:table-cell; position:relative; width:361px; height:46px; vertical-align:middle; text-align:center; line-height:10px; font-size:10px; font-family:\"Trebuchet MS\"; font-weight:bold; color:#000000;}"}],"bOffBottom":0 ,"td": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAW4AAAAzCAYAAAC3xeISAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAABgSURBVHhe7cExAQAAAMKg9U9tDQ8gAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAPtUAI+oAAeT9dYEAAAAASUVORK5CYII="  ,"fd": "" ,"p": "M 0.000000 0.000000 L 365.000000 0.000000 L 365.000000 50.000000 L 0.000000 50.000000 L 0.000000 0.000000 z"}
+,
+"image24257":{"x":40,"y":397,"w":89,"h":85,"bOffBottom":0,"i":"images/unj.png"}
+,
+"text24431":{"x":168,"y":43,"w":451,"h":93,"txtscale":100,"bOffBottom":0}
+,
+"text24677":{"x":131,"y":418,"w":533,"h":72,"txtscale":100,"bOffBottom":0}
+,
+"text24716":{"x":533,"y":189,"w":239,"h":48,"txtscale":100,"bOffBottom":0}
+,
+"entry26092":{"x":593,"y":212,"w":150,"h":27,"fsize":16,"bOffBottom":0}
+,
+"RCDResetQuestion":function(){
+try{if(window.dragMgr)window.dragMgr.clearDropZones();}catch(e){if(e&&e.message)console.log(e.message);}
+}
+,
+"RCDResultResize":function(){}
+,"preload":['images/cisco_society_twitter_1200x600_flat.gif']
+}}
